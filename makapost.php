0cbada262c9ef932152f80e1c4e1e094567fda7d
Joe Bacal <bacalj@gmail.com>
2016-03-29 09:04:20 -0400

    some comments

--- makapost.php
+++ makapost.php
@@ -8,6 +8,11 @@ License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
+/*
+  Make a new private post on Ninja Form submission
+  Write everything into the content of the post
+*/
+
 function add_makapost(){
   add_action( 'ninja_forms_post_process', 'maka_function' );
 }
@@ -19,6 +24,7 @@ function maka_function(){
 
   $all_fields = $ninja_forms_processing->get_all_fields();
 
+  //pull all the fields as keystring -> valuestring into an array
   $content_as_array = [];
   if( is_array( $all_fields ) ){
     foreach( $all_fields as $field_id => $user_value ){
@@ -29,19 +35,18 @@ function maka_function(){
       }
     }
 
-    // Gather post data.
+    // Smoosh array into content and add a title for the post
     $maka_content = implode('<br>', $content_as_array);
     $form_title_string = $ninja_forms_processing->data['form']['form_title'];
     $datestring = date('m j y');
 
+    // set up and create the post
     $new_post = array(
-        'post_title'    => $form_title_string . ' submission on ' . $datestring,
+        'post_title'    => $form_title_string . ' submission | ' . $datestring,
         'post_content'  => $maka_content,
         'post_status'   => 'private'
     );
 
     wp_insert_post( $new_post );
   }
-
-  //die();
 }
